0b46ad3d77283a4495d0b546a2b8065c854d1992
kenberkeley <kenberkeley@foxmail.com>
2016-08-04 19:52:41 +0800

    xhr config add comment: do not add ending slash

--- src/services/xhr/config.js
+++ src/services/xhr/config.js
@@ -2,6 +2,8 @@
 // 更多配置请根据业务逻辑自行实现
 
 // 后端API地址，最好以http(s)://打头
+// 末尾请勿加 / ，因为是如下拼接的
+// 'http://localhost:9000' + '/login'
 export const rootPath = 'http://localhost:9000'
 
 export const errHandler = (e) => {
