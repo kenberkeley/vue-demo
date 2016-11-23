8a50f925e2b10bb8c74885a3c35a11232d244fd1
Ken Berkeley <kenberkeley@foxmail.com>
2016-11-23 17:47:18 +0800

    fix a bug...

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -68,7 +68,7 @@ module.exports = {
       }
     }, {
       test: /\.(woff2?|eot|ttf|otf)$/,
-      loader: 'fonts/url-loader?limit=10240&name=[name]-[hash:6].[ext]'
+      loader: 'url-loader?limit=10240&name=[name]-[hash:6].[ext]'
     }]
   },
   vue: {
